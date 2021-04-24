ed33fe11361da8e2a9196b24cb38e6a196d5c930
Luiz.venancio <luiznetto@protonmail.com>
2021-04-24 17:48:14 -0300

    Feature & Refactor: First day

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+import greetingImg from './img/watering.png';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {'\n'}suas plantas {'\n'}de forma fácil
+      </Text>
+      <Image style={styles.image} source={greetingImg} resizeMode="contain" />
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar
+      </Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>{'>'}</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 38,
+    color: '#32B768',
+  },
+  image: {
+    height: Dimensions.get('window').width * 0.7,
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: '#32B768',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    width: 56,
+    height: 56,
+    backgroundColor: '#32B768',
+  },
+  buttonText: {
+    fontSize: 24,
+    color: '#FFFFFF',
+  },
+});
+
+export default App;
